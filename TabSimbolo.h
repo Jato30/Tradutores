8ea217cdaea5adadf2db9b03e2d0752de3136276
Daniel Marcos <DanielM.B.Barbosa@hotmail.com>
2019-09-10 22:09:08 -0300

    Implementa tab de simbolos e utiliza

--- TabSimbolo.h
+++ TabSimbolo.h
@@ -1,115 +1,147 @@
-#infndef __TABSIMBOLO_H__
+#ifndef __TABSIMBOLO_H__
 #define __TABSIMBOLO_H__
 
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct Simbolo{
-  char* valor;
-  int qtd;
-  int chave;
-  struct Simbolo *prox;
+	char* valor;
+	int qtd;
+	int chave;
+	struct Simbolo *prox;
 } Simbolo;
 
 typedef Simbolo* TabSimbolos;
 
 void cria(TabSimbolos* raiz);
-int buscaTab(TabSimbolos raiz, char* valor);
-void insere(TabSimbolos* raiz, char* valor);
-void printTab(TabSimbolos raiz);
+int buscaTab(TabSimbolos* raiz, char* valor);
+int checa_erro(char* valor, int lin, int col);
+void insere(TabSimbolos* raiz, char* valor, int lin, int col);
+void printTab(TabSimbolos* raiz);
 
 void cria(TabSimbolos* raiz){
-  *raiz = NULL;
-  return 0;
+	*raiz = NULL;
 }
 
-int buscaTab(TabSimbolos raiz, char* valor){
-   if(raiz == NULL){
-      printf("\nTabela vazia");
-      return 0;
-   }
-    else{
-        TabSimbolos atual;
-       atual = *raiz;
-       while(strcmp(atual->valor, valor) != 0){
-           atual = atual->prox;
-           if(atual == NULL){
-             printf("Nenhuma ocorrencia repetida.\n");
-             return 0;
-            }
-       }
-       // aqui se pode incrementar um campo que diz quantas ocorrencias
-       atual->qtd++;
-       return 1;
-    }
+// RETORNO: retorna a chave do elemento repetido, -1 se nao houver repeticao
+int buscaTab(TabSimbolos* raiz, char* valor){
+	if(*raiz == NULL){
+		// printf("\nTabela vazia\n");
+		return -1;
+	}
+	else{
+		TabSimbolos atual;
+		atual = *raiz;
+		while(atual != NULL){
+			if(strcmp(atual->valor, valor) == 0){
+				// printf("\tJa existe %s na tabela de simbolos\n", valor);
+				return atual->chave;
+			}
+			atual = atual->prox;
+		}
+
+		// printf("Nenhuma ocorrencia repetida.\n");
+		return -1;
+	}
 }
 
-void insere(TabSimbolos* raiz, char* valor){
-  
-    // trata erro de tamanho
-    if(strlen(valor) > 33){
-        printf("Tamanho excede o maximo %d>33. valor sera truncado.\n", strlen(valor));
-        char aux_valor[33];
-        int i = 0;
-        for(i = 0; i < 33; i++){
-            aux_valor[i] = valor[i];
-        }
-        valor[33] = '\0';
-        valor = NULL;
-        free(valor);
-        valor = (char*) malloc(sizeof(char*) * 33);
-        strcpy(valor, aux_valor);
-    }
+// RETORNO: 0 nenhnum erro, 1 erro corrigido, 2 erro nao corrigido
+int checa_erro(char* valor, int lin, int col){
+	int erro = 0;
 
-    if(buscaTab(*raiz, valor) == 1){
-        printf("valor ja existente na tabela de simbolos.\n");
-    }
-    else{
-    
-        Simbolo* novo; 
-        Simbolo* atual;
-        int aux_chave = 0;
+	// ERRO DE TAMANHO
+	{
+		int tam = (int) strlen(valor);
+		if(tam > 33){
+			erro = 1;
+			printf("\t[WARNING]: Tamanho excede o maximo %d>33. valor sera truncado. Linha[%d:%d]\n", tam, lin+1, col);
+			char aux_valor[33];
+			int i = 0;
+			for(i = 0; i < 33; i++){
+				aux_valor[i] = valor[i];
+			}
+			valor[33] = '\0';
+			valor = NULL;
+			free(valor);
+			valor = (char*) malloc(sizeof(char*) * 33);
+			strcpy(valor, aux_valor);
+		}
+	}
 
-        novo = (Simbolo*) malloc(sizeof(Simbolo));
-        if(novo == NULL){
-            printf("Falta Memoria\n");
-        }
-        strcpy(novo->valor, valor);
-        novo->qtd = 0;
-        novo->prox = NULL;
+	// ERRO DE IF
+	{
+		if(strcmp("fi", valor) == 0){
+			printf("\t[WARNING]: em vez de %s, nao quis dizer if? Linha[%d:%d]\n", valor, lin+1, col);
+		}
+	}
 
-        if(*raiz == NULL){  //Lista vazia
-            novo->chave = 0;
-            *raiz = novo;
-        }
-        else{
-            atual = *raiz;   /*@ Primeiro elemento*/
-
-            while(atual->prox != NULL){
-                aux_chave++;
-                atual = atual->prox;
-            }
-            atual->prox = novo;
-            novo->chave = aux_chave + 1;
-        }
-    }
+	return 0;
 }
 
-void printTab(TabSimbolos raiz){
-    if(raiz == NULL){
-       printf("\nTabela vazia");
-    }
-    else{
-        TabSimbolos atual;
-        atual = *raiz;
-        printf("\n\nTABELA DE SIMBOLOS ########\n\n");
-        printf("Chave\t\tQuantidade\t\tValor\n");
-        while(atual != NULL){
-            printf("%d\t\t\t%d\t\t%s\n", atual->chave, atual->qtd, atual->valor);
-            atual = atual->prox;
-                 
-        }
-    }
+void insere(TabSimbolos* raiz, char* valor, int lin, int col){
+
+	int busca = buscaTab(raiz, valor);
+	if(busca == -1){
+		if(*raiz == NULL){  //Lista vazia
+			*raiz = (TabSimbolos) malloc(sizeof(Simbolo));
+			(*raiz)->valor = (char*) malloc(sizeof(char) * strlen(valor));
+			strcpy((*raiz)->valor, valor);
+			(*raiz)->chave = 1;
+			(*raiz)->qtd = 1;
+			(*raiz)->prox = NULL;
+		}
+		else{
+			TabSimbolos ultimoLista;
+			int aux_chave = 1;
+
+			ultimoLista = *raiz;
+			while(ultimoLista->prox != NULL){
+				aux_chave++;
+				ultimoLista = ultimoLista->prox;
+			}
+			aux_chave++;
+
+			ultimoLista->prox = (TabSimbolos) malloc(sizeof(Simbolo));
+			ultimoLista->prox->valor = (char*) malloc(sizeof(char) * strlen(valor));
+			strcpy(ultimoLista->prox->valor, valor);
+			ultimoLista->prox->chave = aux_chave;
+			ultimoLista->prox->qtd = 1;
+			ultimoLista->prox->prox = NULL;
+		}
+
+	}
+	else{
+		TabSimbolos atual;
+		atual = *raiz;   /*@ Primeiro elemento*/
+
+		while(atual->prox != NULL){
+			if(atual->chave == busca){
+				atual->qtd++;
+				break;
+			}
+			atual = atual->prox;
+		}
+	}
+
+}
+
+void printTab(TabSimbolos* raiz){
+	printf("\n\nTABELA DE SIMBOLOS ########\n\n");
+
+	if(*raiz == NULL){
+		printf("\nTabela de Simbolos vazia\n");
+	}
+	else{
+		TabSimbolos atual;
+		atual = *raiz;
+		printf("Chave\t\tQuantidade\t\tValor\n");
+
+		while(atual != NULL){
+			printf("%d\t\t%d\t\t\t%s\n", atual->chave, atual->qtd, atual->valor);
+			atual = atual->prox;
+		}
+	}
+
 }
 
 
